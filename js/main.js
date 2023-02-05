faa3a752c6aabc2a71e71ef352408f483fd3bb8f
ksenia-5 <xenia.germanovich@gmail.com>
2023-02-05 10:26:15 +0000

    Adding interactive buttons on getting-started page

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function uncoverToolGuide() {
+    let htmlToolGuide = "<p>This is a step by step guide</p>";
+    document.getElementById("guide-to-tools").innerHTML = htmlToolGuide;
+}
+
+let toolGuideButton = document.getElementById("tool-guide");
+toolGuideButton.addEventListener("click",uncoverToolGuide);
